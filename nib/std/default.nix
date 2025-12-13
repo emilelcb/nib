831203eccedebbb009c745f83a062d69aea63cf3
Emile Clark-Boman <emile@agribit.com.au>
2025-12-13 22:00:34 +1000

    create submodule design pattern

--- nib/std/default.nix
+++ nib/std/default.nix
@@ -1,4 +1,12 @@
-builtins.listToAttrs [
-  (import ./attrs.nix)
-  (import ./lists.nix)
-]
+{}: let
+  attrs = import ./attrs.nix {inherit lists;};
+  lists = import ./lists.nix {};
+  result = import ./lists.nix {inherit lists;};
+in
+  builtins.listToAttrs [
+    # submodule is included directly to this module (ie self.myFunc)
+
+    # submodule content is accessible first by submodule name
+    # then by the name of the content (ie self.submodule.myFunc)
+    {inherit attrs lists result;}
+  ]
