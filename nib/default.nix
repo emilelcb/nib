c7812b57f3a7e42b3f7a1731cd6e76c3d243e90e
Emile Clark-Boman <eclarkboman@gmail.com>
2025-12-13 13:16:48 +1000

    add mkSys/mkUSys proposal for simplified system management

--- nib/default.nix
+++ nib/default.nix
@@ -1,3 +1,26 @@
-{...}: {
-  sys = import ./sys {};
-}
+{systems, ...}: let
+  std = import ./std;
+in
+  std
+  // {
+    # === External Functions ===
+    withPkgs = repo: config: system:
+      import repo {
+        inherit system;
+      }
+      // config;
+
+    mkSys = input: {
+      forAllSystems = f:
+        std.genAttrs systems (
+          system: f system input.pkgs
+        );
+    };
+
+    mkUSys = input: {
+      forAllSystems = f:
+        std.genAttrs systems (
+          system: f system input.pkgs input.upkgs
+        );
+    };
+  }
