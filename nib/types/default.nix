d43af1efac5b7bf254e593e2930991122669be0a
Emile Clark-Boman <emile@agribit.com.au>
2025-12-14 14:36:39 +1000

    fix result.nix used instead of res.nix

--- nib/types/default.nix
+++ nib/types/default.nix
@@ -2,14 +2,14 @@
   attrs = import ./attrs.nix args;
   fault = import ./fault.nix args;
   lists = import ./lists.nix args;
-  result = import ./result.nix args;
+  res = import ./res.nix args;
 in
   attrs.mergeAttrsList [
     # submodule is included directly to this module (ie self.myFunc)
     attrs
     fault
     lists
-    result
+    res
 
     # submodule content is accessible first by submodule name
     # then by the name of the content (ie self.submodule.myFunc)
