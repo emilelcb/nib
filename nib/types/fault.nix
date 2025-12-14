94926061606f2e31dd7175ba1d03edb93cbfcb09
Emile Clark-Boman <eclarkboman@gmail.com>
2025-12-14 17:46:44 +1000

    use _NAME_ convention

--- nib/types/fault.nix
+++ nib/types/fault.nix
@@ -3,14 +3,14 @@ with builtins; rec {
   # Fault Monad
   # Wrapper around an error (ie builtins.abort)
   Fault = error: {
-    error = error;
+    _error_ = error;
   };
 
   # Pattern Matching
-  isFault = F: attrNames F == ["error"];
+  isFault = F: attrNames F == ["_error_"];
 
   # Unwrap (Monadic Return Operation)
-  unwrapFault = F: F.error;
+  unwrapFault = F: F._error_;
 
   # Map (Monadic Bind Operation)
   mapFault = f: F: Fault (f (unwrapFault F));
