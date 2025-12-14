1ae1301a144ac1f74c8cc2fc5a75803132e7858f
Emile Clark-Boman <eclarkboman@gmail.com>
2025-12-15 00:14:04 +1000

    implement Terminal

--- nib/types/default.nix
+++ nib/types/default.nix
@@ -2,10 +2,12 @@
   fault = import ./fault.nix args;
   maybe = import ./maybe.nix args;
   res = import ./res.nix args;
+  terminal = import ./terminal.nix args;
 in
   nib.std.mergeAttrsList [
     # submodule is included directly to this module (ie self.myFunc)
     fault
     maybe
     res
+    terminal
   ]
