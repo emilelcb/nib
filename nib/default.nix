39e7ca683f979b4596e071775df54da4ede4647e
Emile Clark-Boman <eclarkboman@gmail.com>
2025-12-18 11:25:46 +1000

    module: sys.nix

--- nib/default.nix
+++ nib/default.nix
@@ -1,52 +1,21 @@
 {systems, ...}: let
   # TODO: move this to a new module
   mkMod' = args: mod: import mod args;
-  mkMod = mkMod' {inherit nib;};
+  mkMod = mkMod' {inherit systems nib;};
 
   std = mkMod ./std;
   types = mkMod ./types;
   parse = mkMod ./parse;
   panic = mkMod ./panic.nix;
+  sys = mkMod ./sys.nix;
 
   nib = std.mergeAttrsList [
     # submodule content is accessible first by submodule name
     # then by the name of the content (ie self.submodule.myFunc)
     {inherit std types panic parse;}
 
-    # submodule is included directly to this module (ie self.myFunc)
-
-    # this module
-    {
-      # === External Functions ===
-      withPkgs = repo: config: system:
-        import repo {
-          inherit system;
-        }
-        // config;
-
-      mkSys = input: let
-        # function taking a system as argument
-        pkgsFor = input.pkgs;
-      in {
-        inherit pkgsFor;
-        forAllSystems = f:
-          std.genAttrs systems (
-            system: f system (pkgsFor system)
-          );
-      };
-
-      mkUSys = input: let
-        # functions taking a system as argument
-        pkgsFor = input.pkgs;
-        upkgsFor = input.upkgs;
-      in {
-        inherit pkgsFor upkgsFor;
-        forAllSystems = f:
-          std.genAttrs systems (
-            system: f system (pkgsFor system) (upkgsFor system)
-          );
-      };
-    }
+    # submodule content accessible directly (ie self.myFunc)
+    sys
   ];
 in
   nib
