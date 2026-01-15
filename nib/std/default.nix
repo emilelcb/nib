8908b0cacdd4b73b8268e134b019daf7ef4cbc68
Emile Clark-Boman <emile@agribit.com.au>
2026-01-15 13:34:07 +1000

    add std/strings.nix

--- nib/std/default.nix
+++ nib/std/default.nix
@@ -2,11 +2,13 @@
   attrs = import ./attrs.nix args;
   functions = import ./functions.nix args;
   lists = import ./lists.nix args;
+  strings = import ./strings.nix args;
   trivial = import ./trivial.nix args;
 in
   attrs.mergeAttrsList [
     attrs
     functions
     lists
+    strings
     trivial
   ]
